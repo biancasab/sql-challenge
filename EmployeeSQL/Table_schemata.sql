ddf498646b0d5e56f82457a022ae079b690d8d7b
biancasab <biancasabino@Biancas-MacBook-Air.local>
2024-02-29 22:16:08 -0800

    submission for Module 9 challenge

--- EmployeeSQL/Table_schemata.sql
+++ EmployeeSQL/Table_schemata.sql
@@ -1,46 +1,35 @@
 -- Data Engineering
 
--- Create table syntax was exported from QuickDBD but made some changse
+-- Create table syntax was exported from QuickDBD but made some changes
+-- such as adding the DROP TABLE IF EXISTS functions.
+-- I also updated the data type of emp_no from integer to varchar because enountered issues when importing the data to the tables. 
+
+DROP TABLE IF EXISTS titles;
+
+CREATE TABLE titles (
+    title_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    PRIMARY KEY (title_id)
+);
 
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
-    dept_no INTEGER NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(255) NOT NULL,
+    dept_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (dept_no)
-);
-
-
-DROP TABLE IF EXISTS dept_emp;
-
-CREATE TABLE dept_emp (
-    emp_no INTEGER NOT NULL,
-    dept_no INTEGER NOT NULL,
-    PRIMARY KEY (emp_no, dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
-);
-
-DROP TABLE IF EXISTS dept_manager;
-
-CREATE TABLE dept_manager (
-    dept_no INTEGER NOT NULL,
-    emp_no INTEGER NOT NULL,
-    PRIMARY KEY (dept_no, emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE employees (
-    emp_no INTEGER NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
-    hire_date DATE NOT NULL,
+    emp_no VARCHAR(255) NOT NULL,
+    emp_title_id VARCHAR(255) NOT NULL,
+    birth_date VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sex VARCHAR(255) NOT NULL,
+    hire_date VARCHAR(255) NOT NULL,
     PRIMARY KEY (emp_no),
     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
@@ -48,17 +37,36 @@ CREATE TABLE employees (
 DROP TABLE IF EXISTS salaries;
 
 CREATE TABLE salaries (
-    emp_no INTEGER NOT NULL,
+    emp_no VARCHAR(255) NOT NULL,
     salary INTEGER NOT NULL,
     PRIMARY KEY (emp_no, salary),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS dept_manager;
 
-CREATE TABLE titles (
-    title_id VARCHAR NOT NULL,
-    title VARCHAR NOT NULL,
-    PRIMARY KEY (title_id)
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(255) NOT NULL,
+    emp_no VARCHAR(255) NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+DROP TABLE IF EXISTS dept_emp;
+
+CREATE TABLE dept_emp (
+    emp_no VARCHAR(255) NOT NULL,
+    dept_no VARCHAR(255) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+--Check tables
+SELECT * FROM titles;
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# sql-challenge
+# Module 9 Challenge
+
+This is my submission for the Module 9 challenge of the UCI Bootcamp. 
+
+Data Modeling
+I used https://www.quickdatabasediagrams.com/ to create an entity relationship diagram based on the csv files provided (departments, dept_emp, dept_manager, employees, salaries, titles)
+
+Data Engineering
+I exported the entity relationship diagram to PostgreSQL using the same link. I made a few changes such as adding the DROP TABLE IF EXISTS functions. This can be found in the Table_schemata.sql file.
+
+Data Analysis
+From the data provided, I created specific tables using SQL based on the list provided. This can be found in the queries.sql file. 
